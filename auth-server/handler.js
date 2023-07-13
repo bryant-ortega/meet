61b57fe4602d1c9406a72bdf8d58f1cbf91f4eac
Bryant Ortega <kalicorose@gmail.com>
2023-07-13 00:35:04 -0700

    created getAccessToken Function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -21,12 +21,7 @@ const oAuth2Client = new google.auth.OAuth2(
 
 //created and exported using Node.js module.exports.
 module.exports.getAuthURL = async () => {
-    /**
-     *
-     * Scopes array is passed to the `scope` option.
-     *
-     */
-
+    /* * Scopes array is passed to the `scope` option.*/
      //allows you to seamlessly retrieve an access token, refresh it, and retry the request. 
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: "offline",
@@ -43,4 +38,41 @@ module.exports.getAuthURL = async () => {
             authUrl,
         }),
     };
+};
+
+module.exports.getAccessToken = async event => {
+    // Decode authorization code extracted from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+    return new Promise((resolve, reject) => {
+        /**
+         *  Exchange authorization code for access token with a “callback” after the exchange,
+         *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+         */
+
+        oAuth2Client.getToken(code, (error, response) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(response);
+        });
+    })
+        .then(results => {
+            // Respond with OAuth token
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body: JSON.stringify(results),
+            };
+        })
+        .catch(error => {
+            // Handle error
+            return {
+                statusCode: 500,
+                body: JSON.stringify(error),
+            };
+        });
 };
