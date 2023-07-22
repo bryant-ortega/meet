10134b9c74700cb58b5e74c28ddae0c2fd82e533
Bryant Ortega <kalicorose@gmail.com>
2023-07-21 18:01:38 -0700

    passed 1st integration test in EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,8 +1,9 @@
 // src/__tests__/EventList.test.js
 
-import { render } from "@testing-library/react";
-import EventList from "../components/EventList";
+import { render, within, waitFor } from "@testing-library/react";
 import { getEvents } from "../api";
+import EventList from "../components/EventList";
+import App from "../App";
 
 describe("<EventList /> component", () => {
     let EventListComponent;
@@ -21,4 +22,18 @@ describe("<EventList /> component", () => {
             allEvents.length
         );
     });
+});
+
+//INTEGRATION TESTS
+describe("<EventList /> integration", () => {
+  test("renders a list of 32 events when the api is mounted and rendered", async () => {
+    const AppComponent = render(<App />);
+    const AppDom = AppComponent.container.firstChild;
+    const EventListDom = AppDom.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDom).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(32);
+    });
+  });
+  
 });
