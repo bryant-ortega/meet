e50540a49899c51a2c729fbf5f28d01f1edf219c
Bryant Ortega <kalicorose@gmail.com>
2023-08-02 15:50:49 -0700

    updated app.css

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -8,6 +8,31 @@ const EventGenresChart = ({ events }) => {
     const [data, setData] = useState([]);
     const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
 
+    const renderCustomizedLabel = ({
+        cx,
+        cy,
+        midAngle,
+        outerRadius,
+        percent,
+        index,
+    }) => {
+        const RADIAN = Math.PI / 180;
+        const radius = outerRadius;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+        const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+        return percent ? (
+            <text
+                x={x}
+                y={y}
+                fill="#8884d8"
+                textAnchor={x > cx ? "start" : "end"}
+                dominantBaseline="central"
+            >
+                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        ) : null;
+    };
+
     useEffect(() => {
         setData(getData());
     }, [`${events}`]);
@@ -33,7 +58,7 @@ const EventGenresChart = ({ events }) => {
                     dataKey="value"
                     fill="#8884d8"
                     labelLine={false}
-                    label
+                    label={renderCustomizedLabel}
                     outerRadius={130}
                 />
             </PieChart>
