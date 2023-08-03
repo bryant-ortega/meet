1d04f48cc724d2d6d820b548bd7d1801a18c9dd2
Bryant Ortega <kalicorose@gmail.com>
2023-08-02 23:39:10 -0700

    attempted to fix outer radius

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -51,7 +51,7 @@ const EventGenresChart = ({ events }) => {
     };
 
     return (
-        <ResponsiveContainer width="97%" height={400}>
+        <ResponsiveContainer width="99%" height={400}>
             <PieChart>
                 <Pie
                     data={data}
@@ -59,7 +59,7 @@ const EventGenresChart = ({ events }) => {
                     fill="#8884d8"
                     labelLine={false}
                     label={renderCustomizedLabel}
-                    outerRadius={130}
+                    outerRadius={120}
                 />
             </PieChart>
         </ResponsiveContainer>
